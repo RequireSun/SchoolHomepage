0f4ff006c1fdf555ad6559e55e2b75656a38081b
Kelvin-09 <kelvinlpsun@hotmail.com>
2014-12-12 15:39:15 +0800

    create branch kelvin_01

---
+++ WebSite/App_Code/InformationDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// InformationDAO 的摘要说明
+/// </summary>
+public class InformationDAO : BasicDAO
+{
+    public string GetInformation(int categoryId) 
+    {
+        if(null == categoryId || 1 > categoryId)
+        {
+            return string.Empty;
+        }
+
+        string sql = "SELECT article FROM information WHERE category_id = @CategoryId";
+        SqlParameter[] parameters = { BasicDAO.MakeInParameter("@CategoryId", SqlDbType.Int, -1, categoryId) };
+
+        return base.GetSingleDataInColumn(sql, parameters, "article");
+    }
+}
---
+++ WebSite/ManagementInformationModify.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ManagementInformationModify : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string categoryType = Request.QueryString["id"];
+        int categoryId = Convert.ToInt32(categoryType);
+        if (null == categoryType || categoryType.Equals(string.Empty) || null == categoryId)
+        {
+            this.showFalseMessage("请输入正确的请求代号！");
+            return;
+        }
+    }
+}
